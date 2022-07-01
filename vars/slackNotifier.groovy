65ff78b813d9b1369694235bc0348d110370e07d
Cedric Gerbaud <cedric.gerbaud@protonmail.com>
2022-07-01 16:41:08 +0200

    slack blocks test

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,8 +1,33 @@
 #!/usr/bin/env groovy
 
+blocks = [
+  [
+    "type": "section",
+    "text": [
+			"type": "mrkdwn",
+			"text": "<a href='https://www.thalesgroup.com/en'><img src='https://img.shields.io/badge/Thales-100%25-blue.svg?style=for-the-badge&logo=airbus'/></a>CONGRATULATION: A320 est en route, vive Thales !"
+		]
+  ],
+  [
+    "type": "divider"
+  ],
+  [
+    "type": "section",
+    "text": [
+			"type": "mrkdwn",
+			"text": "Décollage imminent"
+		],
+    "accessory": [
+			"type": "image",
+			"image_url": "https://media3.giphy.com/media/5XCWLH6ovlsiISpU2U/giphy.gif",
+			"alt_text": "take off"
+		]
+  ],
+]
+
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "<a href='https://www.thalesgroup.com/en'><img src='https://img.shields.io/badge/Thales-100%25-blue.svg?style=for-the-badge&logo=airbus'/></a>CONGRATULATION: A320 est en route, vive Thales ! more info ${env.BUILD_URL}"
+    slackSend(channel: "test_notif_jenkins", blocks: blocks),
   }
   else if( buildResult == "FAILURE" ) { 
     slackSend color: "danger", message: "BAD NEWS:Job ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed ! more info ${env.BUILD_URL}"
